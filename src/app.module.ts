d8ac680e3091c5fd2ed0daf7ff70c575f386490b
oko_jin <dswvgw6412@naver.com>
2025-03-01 20:39:56 +0900

    feat: appController 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SnacksModule } from './snacks/snacks.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [SnacksModule, PrismaModule],
+  controllers: [AppController],
 })
 export class AppModule {}
